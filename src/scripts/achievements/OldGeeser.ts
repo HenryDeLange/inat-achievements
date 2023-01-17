0648520371cfd23feea5558b1ba08326acebcc46
Henry <36891536+HenryDeLange@users.noreply.github.com>
2023-01-17 07:00:29 +0200

    updated versions and fixed date bug

--- src/scripts/achievements/OldGeeser.ts
+++ src/scripts/achievements/OldGeeser.ts
@@ -13,21 +13,20 @@ export default new AchievementData(
     () => [TAXA],
     (iNatObsJSON: Observation) => {
         for (let taxonID of iNatObsJSON?.taxon?.ancestor_ids ?? []) {
-            if (TAXA === taxonID
-                    && iNatObsJSON?.observed_on_details?.year
-                    && iNatObsJSON?.observed_on_details?.month
-                    && iNatObsJSON?.observed_on_details?.day) {
-                const obsDate = new Date(
-                    iNatObsJSON.observed_on_details.year,
-                    iNatObsJSON.observed_on_details.month,
-                    iNatObsJSON.observed_on_details.day);
-                const currentDate = new Date();
-                if (obsDate < currentDate) {
-                    const obsYearsAgo = differenceInYears(currentDate, obsDate);
-                    if (obsYearsAgo > maxYearsAgo) {
-                        const delta = obsYearsAgo - maxYearsAgo;
-                        maxYearsAgo = obsYearsAgo;
-                        return delta;
+            if (TAXA === taxonID) {
+                const obsYear = iNatObsJSON?.observed_on_details?.year ?? -1;
+                const obsMonth = iNatObsJSON?.observed_on_details?.month ?? -1;
+                const obsDay = iNatObsJSON?.observed_on_details?.day ?? -1;
+                if (obsYear >= 0 && obsMonth >= 0 && obsDay >= 0) {
+                    const obsDate = new Date(obsYear, obsMonth, obsDay);
+                    const currentDate = new Date();
+                    if (obsDate < currentDate) {
+                        const obsYearsAgo = differenceInYears(currentDate, obsDate);
+                        if (obsYearsAgo > maxYearsAgo) {
+                            const delta = obsYearsAgo - maxYearsAgo;
+                            maxYearsAgo = obsYearsAgo;
+                            return delta;
+                        }
                     }
                 }
             }
